87b25db6ccd5e839d8eba73de6c976779f6c1d5b
mikermillersd <148576853+mikermillersd@users.noreply.github.com>
2024-07-14 21:27:45 -0700

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -87,7 +87,7 @@ sudo apt-get update
 
 sudo apt-get install build-essential
 
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+sudo apt-get install libncursesw5-dev libssl-dev \
 libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev curl
 
 echo "Done."
